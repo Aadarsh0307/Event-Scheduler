e83c2415c70deeae967a30eedcd2913e01511919
Aadarsh Rai <90710951+Aadarsh0307@users.noreply.github.com>
2025-07-24 22:44:42 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import EventScheduler from './components/EventScheduler'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Weekly/Daily Event Scheduler</h1>
+      <EventScheduler />
+    </div>
+  )
+}
+
+export default App
